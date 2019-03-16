01a99bdc213126f9e00d25241d1703506f0ede5a
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-03-16 15:09:47 +0100

    Added app component with heading

---
+++ sirkus-media/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header headline="Vi hjelper deg" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
