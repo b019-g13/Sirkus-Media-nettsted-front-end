1f832d330ee50231e5c81eaef273c85fa7c0ca23
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-03-21 16:04:58 +0100

    Gather Header component-files in folder. Update paths that imports header

---
+++ src/components/header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./header.scss";
+
+class HeaderComponent extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <section className="header-left">
+          <h1>{this.props.h1}</h1>
+          <p>{this.props.headerText}</p>
+          <button>{this.props.headerButton}</button>
+        </section>
+        <section className="header-right">
+          <img
+            src={this.props.headerIllustration.url}
+            alt=""
+            className="headerIcon"
+          />
+        </section>
+      </header>
+    );
+  }
+}
+
+export default HeaderComponent;
