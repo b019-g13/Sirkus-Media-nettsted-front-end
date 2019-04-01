f9c90429e0814d39e3894e89d473f9a41de2a7b4
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-01 17:17:57 +0200

    Implement children component

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,8 +2,10 @@ import React from "react";
 import Axios from "axios";
 import HeaderComponent from "./header/Header";
 import HeaderMenu from "./header-menu/HeaderMenu";
-import ActionBoxFrontPage from "./action-box/ActionBoxFrontPage";
+import ActionBoxFrontPage from "./action-box-front-page/ActionBoxFrontPage";
 import AboutUs from "./about-us/AboutUs";
+import Process from "./process/Process";
+import ProcessStep from "./process-step/ProcessStep";
 
 class Page extends React.Component {
   componentWillMount() {
@@ -19,9 +21,144 @@ class Page extends React.Component {
     this.getMenus();
   }
 
+  // Definerer alle komponenter
+  AllComponents = {
+    headercomponent: HeaderComponent,
+    actionboxfrontpage: ActionBoxFrontPage,
+    aboutus: AboutUs,
+    process: Process,
+    processstep: ProcessStep
+  };
+
   getPageComponents() {
     Axios.get("http://localhost:8000/api/v1/pages/" + this.props.page.id)
       .then(response => {
+        const childComps = [
+          {
+            id: 123123,
+            name: "ProcessStep",
+            slug: "processstep",
+            fields: [
+              {
+                id: 1340,
+                name: "Process illustration",
+                slug: "processIllustration",
+                value: [
+                  {
+                    id: 121,
+                    attribute_alt: "Illustrasjon i prosess",
+                    url: "/images/header-illustration.png"
+                  }
+                ]
+              },
+              {
+                id: 45453,
+                name: "Process step heading",
+                slug: "processStepHeading",
+                value: "1. Vi hjelper deg"
+              },
+              {
+                id: 45453,
+                name: "Process step text",
+                slug: "processStepText",
+                value:
+                  "Vi gjør alt fra A til Å Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis magna imperdiet, rhoncus lorem sed, laoreet arcu. Sed at malesuada tortor, sed vehicula augue. Curabitur feugiat at justo vitae interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              }
+            ]
+          },
+          {
+            id: 123123,
+            name: "ProcessStep",
+            slug: "processstep",
+            fields: [
+              {
+                id: 1340,
+                name: "Process illustration",
+                slug: "processIllustration",
+                value: [
+                  {
+                    id: 121,
+                    attribute_alt: "Illustrasjon i prosess",
+                    url: "/images/header-illustration.png"
+                  }
+                ]
+              },
+              {
+                id: 45453,
+                name: "Process step heading",
+                slug: "processStepHeading",
+                value: "2. Hei på deg"
+              },
+              {
+                id: 45453,
+                name: "Process step text",
+                slug: "processStepText",
+                value:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis magna imperdiet, rhoncus lorem sed, laoreet arcu. Sed at malesuada tortor, sed vehicula augue. Curabitur feugiat at justo vitae interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              }
+            ]
+          },
+          {
+            id: 12313423,
+            name: "ProcessStep",
+            slug: "processstep",
+            fields: [
+              {
+                id: 1340,
+                name: "Process illustration",
+                slug: "processIllustration",
+                value: [
+                  {
+                    id: 121,
+                    attribute_alt: "Illustrasjon i prosess",
+                    url: "../images/header-illustration.png"
+                  }
+                ]
+              },
+              {
+                id: 45453,
+                name: "Process step heading",
+                slug: "processStepHeading",
+                value: "3. Hade"
+              },
+              {
+                id: 45453,
+                name: "Process step text",
+                slug: "processStepText",
+                value:
+                  "Sed id egestas lorem. Ut orci ipsum, finibus id condimentum in, elementum quis dolor. Quisque vitae arcu pretium, tincidunt leo eu, tempor enim. Quisque non erat justo."
+              }
+            ]
+          }
+          // {
+          //   id: 1231423,
+          //   name: "ProcessStep",
+          //   slug: "processstep",
+          //   fields: [
+          //     {
+          //       id: 10,
+          //       name: "Action box acpl icon",
+          //       slug: "actionBoxACPLIcon",
+          //       value: [
+          //           {
+          //             id: 121,
+          //             attribute_alt: "Illustrasjon i prosess",
+          //             url: "/images/header-illustration.png"
+          //           }
+          //         ]
+          //     },
+          //     {
+          //       id: 45453,
+          //       name: "Mitt field 2",
+          //       slug: "mittField2",
+          //       value: "verdi 31"
+          //     }
+          //   ]
+          // }
+        ];
+
+        response.data.components[5].children = childComps;
+
         this.setState({ pageComponents: response.data.components });
         this.setState({ readyComponents: this.pageSetup() });
       })
@@ -33,38 +170,48 @@ class Page extends React.Component {
       });
   }
 
-  pageSetup() {
-    // Definerer alle komponenter
-    const AllComponents = {
-      headercomponent: HeaderComponent,
-      actionboxfrontpage: ActionBoxFrontPage,
-      aboutus: AboutUs
-    };
+  setupComponent(component, i) {
+    const ReactComponent = this.AllComponents[component.slug];
 
+    if (ReactComponent != null) {
+      let componentFields = {};
+
+      // Looper gjennom alle fields/props til komponenten
+      for (let field = 0; field < component.fields.length; field++) {
+        const slug = component.fields[field].slug;
+        const value = component.fields[field].value;
+
+        componentFields[slug] = value;
+      }
+
+      if (component.children == null) {
+        return (
+          <ReactComponent {...componentFields} key={i + "-" + component.id} />
+        );
+      } else {
+        let componentChildren = [];
+
+        component.children.map((child, j) => {
+          componentChildren.push(this.setupComponent(child, i + "-" + j));
+        });
+
+        return (
+          <ReactComponent
+            {...componentFields}
+            children={componentChildren}
+            key={i + "-" + component.id}
+          />
+        );
+      }
+    }
+  }
+
+  pageSetup() {
     const readyComponents = [];
 
     // Looper igjennom komponentene til siden
-    this.state.pageComponents.map(function(pageComponent, i) {
-      const pageComponentsName = pageComponent.slug;
-
-      // Looper igjennom alle de definerte komponentene
-      Object.keys(AllComponents).map(function(slug) {
-        const AllComponentsName = slug;
-        if (pageComponentsName === AllComponentsName) {
-          const Comp = AllComponents[slug];
-          let componentFields = {};
-
-          // Looper gjennom alle fields/props til komponenten
-          for (let field = 0; field < pageComponent.fields.length; field++) {
-            const slug = pageComponent.fields[field].slug;
-            const value = pageComponent.fields[field].value;
-
-            componentFields[slug] = value;
-          }
-
-          readyComponents.push(<Comp {...componentFields} key={i} />);
-        }
-      });
+    this.state.pageComponents.map((component, i) => {
+      readyComponents.push(this.setupComponent(component, i));
     });
 
     return readyComponents;
@@ -92,7 +239,6 @@ class Page extends React.Component {
   }
 
   render() {
-    // console.log("a", this.state.readyMenus["navTop"]);
     const navTop = this.state.readyMenus.navTop;
     const condition = navTop != null;
     const itsTrue = <HeaderMenu menu={navTop} />;
