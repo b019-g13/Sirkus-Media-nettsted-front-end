626c3fb30c1e0affbce15e7ba5ed28637e63483b
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-08 20:42:34 +0200

    Add contact form heading

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -3,9 +3,12 @@ import React from "react";
 class ContactForm extends React.Component {
   render() {
     return (
-      <form>
-        <input type="text" />
-      </form>
+      <section>
+        <h2>{this.props["overskrift-2"]}</h2>
+        <form>
+          <input type="text" />
+        </form>
+      </section>
     );
   }
 }
