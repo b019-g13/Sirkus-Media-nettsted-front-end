e0f589f8a14077c730c94286d3982c88f3b19d8b
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-09 16:06:22 +0200

    Add fields in footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,27 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="footer">
-        <p>{this.props["tekst"]}</p>
+        <div className="footer-inner">
+          <section className="footer-inner-left">
+            <div>
+              <p>{this.props["tekst"][0]}</p>
+            </div>
+            <div className="footer-inner-left-links">
+              <a href="#">{this.props["intern-link"][0]}</a>
+              <a href="#">{this.props["intern-link"][1]}</a>
+            </div>
+            <p>{this.props["tekst"][1]}</p>
+          </section>
+          <section className="footer-inner-right">
+            {this.props.children.map((children, i) => {
+              return children;
+            })}
+            <a href="#">{this.props["intern-link"][2]}</a>
+          </section>
+        </div>
+        <div className="footer-copyright">
+          <p>{this.props["tekst"][2]}</p>
+        </div>
       </footer>
     );
   }
