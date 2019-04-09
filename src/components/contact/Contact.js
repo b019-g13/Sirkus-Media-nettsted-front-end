7e8ce848d7fdcf77a9c73861b26a1ae431fed538
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-09 16:05:06 +0200

    Import child component. Add map

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "./contact.scss";
+import "../contact-form/contact-form.scss";
 
 class Contact extends React.Component {
   render() {
     return (
-      <article className="Contact">
-        <section>
-          {this.props.children.map((contactform, i) => {
-            return contactform;
-          })}
+      <article className="contact">
+        <h2>{this.props["overskrift-2"]}</h2>
+        {this.props.children.map((contactform, i) => {
+          return contactform;
+        })}
+        <section className="contact-map">
+          <img src="https://i.xdh.no/1adb1ef92f51" />
         </section>
-        <section>{/* Kart */}</section>
       </article>
     );
   }
