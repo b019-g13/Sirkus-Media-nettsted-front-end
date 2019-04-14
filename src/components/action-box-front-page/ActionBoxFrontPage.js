0cea51f275c8e1fd6d024fc9fb685119d48f90ca
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-14 21:15:25 +0200

    Implement actionboxchild component

--- src/components/action-box-front-page/ActionBoxFrontPage.js
+++ src/components/action-box-front-page/ActionBoxFrontPage.js
@@ -3,45 +3,20 @@ import "./action-box.scss";
 
 class ActionBoxFrontPage extends React.Component {
   render() {
+    console.log("hersånn123", this.props.children);
     return (
-      <article className="actionBox">
+      <article className="action-box">
         <h3>{this.props["overskrift-3"]}</h3>
-        <div className="actionBox-top">
-          <section className="actionBox-top-icon-texts">
-            {this.props.children.map((icontext, i) => {
-              return icontext;
+        <div className="action-box-top">
+          <section className="action-box-top-content">
+            {this.props.children.map((children, i) => {
+              return children;
             })}
           </section>
-          <section className="actionBox-top-info">
-            {/* <img
-              src={this.props.actionBoxLeadsIcon.url}
-              alt={this.props.actionBoxLeadsIcon.attribute_alt}
-              className="leadsIcon"
-            /> */}
-            <p className="actionBox-top-info-results">
-              {this.props["tekst"][0]}
-            </p>
-            <p className="actionBox-top-info-results">
-              {this.props["tekst"][1]}
-            </p>
-            <p className="actionBox-top-info-results">
-              {this.props["tekst"][2]}
-            </p>
-          </section>
-          {/* <img
-              src={this.props.actionBoxACPLIcon.url}
-              alt={this.props.actionBoxACPLIcon.attribute_alt}
-              className="leadsIcon"
-            /> */}
-          {/* <img
-              src={this.props.actionBoxCampaignIcon.url}
-              alt={this.props.actionBoxCampaignIcon.attribute_alt}
-              className="leadsIcon"
-            /> */}
         </div>
-        <div className="actionBox-bottom">
+        <div className="action-box-bottom">
           <hr />
-          <section className="actionBox-bottom-content">
+          <section className="action-box-bottom-content">
             <h2>{this.props["overskrift-2"]}</h2>
             <p>{this.props["tekst"][3]}</p>
           </section>
