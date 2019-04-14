45dc99484e005b72b525fb3bdd779762e29511bd
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-14 21:15:01 +0200

    Remove test heading

--- src/components/icon-text/IconText.js
+++ src/components/icon-text/IconText.js
@@ -4,7 +4,6 @@ class IconText extends React.Component {
   render() {
     return (
       <section className="icon-text">
-        <h3>{this.props["overskrift-3"]}</h3>
         <p>{this.props["tekst"]}</p>
       </section>
     );
