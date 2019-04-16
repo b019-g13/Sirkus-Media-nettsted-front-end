dadd24fd0804b13f2a33eb949ba468913a0d6fa6
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-16 17:31:31 +0200

    Remove comment and console log

--- src/components/action-box-front-page/ActionBoxFrontPage.js
+++ src/components/action-box-front-page/ActionBoxFrontPage.js
@@ -3,20 +3,19 @@ import "./action-box.scss";
 
 class ActionBoxFrontPage extends React.Component {
   render() {
-    console.log("hersånn123", this.props.children);
     return (
       <article className="action-box">
         <div className="action-box-inner layout-boxed">
-        <h3>{this.props["overskrift-3"]}</h3>
-        <div className="action-box-top">
-          <section className="action-box-top-content">
+          <h3>{this.props["overskrift-3"]}</h3>
+          <div className="action-box-top">
+            {/* <section className="action-box-top-content"> */}
             {this.props.children.map((children, i) => {
               return children;
             })}
-          </section>
-        </div>
-        <div className="action-box-bottom">
-          <hr />
+            {/* </section> */}
+          </div>
+          <div className="action-box-bottom">
+            <hr />
           <section className="action-box-bottom-content">
             <h2>{this.props["overskrift-2"]}</h2>
             <p>{this.props["tekst"][3]}</p>
