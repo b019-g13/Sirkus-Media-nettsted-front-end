dd5832276a07fb6cf18c8d607e20643043e5ccec
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-18 20:36:11 +0200

    Check if field exists before printing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,25 +3,61 @@ import "./header.scss";
 
 class Header extends React.Component {
   render() {
+    let headerLefth1 = this.props["overskrift-1"];
+    if (headerLefth1) {
+      headerLefth1 = <h1>{headerLefth1}</h1>;
+    }
+
+    let headerLeftP = this.props["tekst"][0];
+    if (headerLeftP) {
+      headerLeftP = <p>{headerLeftP}</p>;
+    }
+
+    let headerLeftButton = this.props["knapp-intern"];
+    if (headerLeftButton) {
+      headerLeftButton = (
+        <a href="#" className="button">
+          {headerLeftButton}
+        </a>
+      );
+    }
+
+    let headerContactInfo1 = this.props["tekst"][1];
+    if (headerContactInfo1) {
+      headerContactInfo1 = <p>{headerContactInfo1}</p>;
+    }
+
+    let headerContactInfo2 = this.props["tekst"][2];
+    if (headerContactInfo2) {
+      headerContactInfo2 = (
+        <p className="contact-number">{headerContactInfo2}</p>
+      );
+    }
+
+    let headerRightImg = this.props["bilde"];
+    if (headerRightImg) {
+      headerRightImg = (
+        <img
+          src={headerRightImg.url_full}
+          alt={headerRightImg.attribute_alt}
+          className="headerIcon"
+        />
+      );
+    }
+
     return (
       <header className="header">
         <div className="header-inner layout-boxed">
           <section className="header-left">
-            <h1>{this.props["overskrift-1"]}</h1>
-            <p>{this.props["tekst"][0]}</p>
-            <button>{this.props["knapp-intern"]}</button>
+            {headerLefth1}
+            {headerLeftP}
+            {headerLeftButton}
             <section className="header-contact-info">
-              <p>{this.props["tekst"][1]}</p>
-              <p className="contact-number">{this.props["tekst"][2]}</p>
+              {headerContactInfo1}
+              {headerContactInfo2}
             </section>
           </section>
-          <section className="header-right">
-            <img
-              src={this.props.bilde.url_full}
-              alt={this.props.bilde.attribute_alt}
-              className="headerIcon"
-            />
-          </section>
+          <section className="header-right">{headerRightImg}</section>
         </div>
       </header>
     );
