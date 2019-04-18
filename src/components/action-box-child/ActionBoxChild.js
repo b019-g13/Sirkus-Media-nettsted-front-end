d2f5dea0d8d4135e6d80bfc58d3223193ee3a0f3
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-18 20:33:10 +0200

    Make a component for actionbox children

--- src/components/action-box-child/ActionBoxChild.js
+++ src/components/action-box-child/ActionBoxChild.js
@@ -2,12 +2,19 @@ import React from "react";
 
 class ActionBoxChild extends React.Component {
   render() {
+    let actionBoxChildP = this.props["tekst"];
+    if (actionBoxChildP) {
+      actionBoxChildP = (
+        <p className="action-box-children-res">{actionBoxChildP}</p>
+      );
+    }
+
     return (
       <section className="action-box-children">
         {this.props.children.map((icontext, i) => {
           return icontext;
         })}
-        <p className="action-box-children-res">{this.props["tekst"]}</p>
+        {actionBoxChildP}
       </section>
     );
   }
