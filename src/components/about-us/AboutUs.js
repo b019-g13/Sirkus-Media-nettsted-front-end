6cfeaea2b5d4d535f480d0ca8dc2fbf63391dd32
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-18 20:31:34 +0200

    Make AboutUs use children component

--- src/components/about-us/AboutUs.js
+++ src/components/about-us/AboutUs.js
@@ -1,21 +1,11 @@
 import React from "react";
-import "./about-us.scss";
-
 class AboutUs extends React.Component {
   render() {
     return (
       <section className="about-us layout-boxed">
-        <section className="about-us-left">
-          <img
-            src={this.props.bilde.url_full}
-            alt={this.props.bilde.attribute_alt}
-          />
-        </section>
-        <section className="about-us-right">
-          <h2>{this.props["overskrift-2"]}</h2>
-          <p>{this.props["tekst"][0]}</p>
-          <p>{this.props["tekst"][1]}</p>
-        </section>
+        {this.props.children.map((children, i) => {
+          return children;
+        })}
       </section>
     );
   }
