396cd151e7823dc157b32eb99248efc3933478ae
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-18 20:35:04 +0200

    Add employee component

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./employee.scss";
+class Employee extends React.Component {
+  render() {
+    let employeeLeftImg = this.props["bilde"];
+    if (employeeLeftImg) {
+      employeeLeftImg = (
+        <img
+          src={employeeLeftImg.url_full}
+          alt={employeeLeftImg.attribute_alt}
+        />
+      );
+    }
+
+    let employeeRighth2 = this.props["overskrift-2"];
+    if (employeeRighth2) {
+      employeeRighth2 = <h2>{employeeRighth2}</h2>;
+    }
+
+    let employeeRightRole = this.props["overskrift-3"];
+    if (employeeRightRole) {
+      employeeRightRole = <p className="employee-role">{employeeRightRole}</p>;
+    }
+
+    let employeeRightP = this.props["tekst"];
+    if (employeeRightP) {
+      employeeRightP = <p>{employeeRightP}</p>;
+    }
+
+    return (
+      <article className="about-us-emp layout-boxed">
+        <section className="about-us-emp-left">{employeeLeftImg}</section>
+        <section className="about-us-emp-right">
+          {employeeRighth2}
+          {employeeRightRole}
+          {employeeRightP}
+        </section>
+      </article>
+    );
+  }
+}
+
+export default Employee;
