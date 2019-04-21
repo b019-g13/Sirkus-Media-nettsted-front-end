bb426d095a76f3adee93fb7d096b661e69d4968a
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-21 17:04:04 +0200

    Add image and check if fields exists in iconlink component

--- src/components/icon-link/IconLink.js
+++ src/components/icon-link/IconLink.js
@@ -1,13 +1,29 @@
 import React from "react";
+import "./icon-link.scss";
 
 class IconLink extends React.Component {
   render() {
-    let iconLink = this.props["intern-link"];
-    if (iconLink) {
-      iconLink = <a href="#">{iconLink}</a>;
+    let iconLinkIcon = this.props["img"];
+    if (iconLinkIcon) {
+      iconLinkIcon = (
+        <img
+          src={iconLinkIcon.url_full}
+          alt={iconLinkIcon.attribute_alt}
+          className="headerIcon"
+        />
+      );
+    }
+    let iconLinkText = this.props["a_ext"];
+    if (iconLinkText) {
+      iconLinkText = <a href="#">{iconLinkText}</a>;
     }
 
-    return <section className="icon-link">{iconLink}</section>;
+    return (
+      <article className="icon-link">
+        <section className="icon-link-icon">{iconLinkIcon}</section>
+        <section className="icon-link-text">{iconLinkText}</section>
+      </article>
+    );
   }
 }
 
