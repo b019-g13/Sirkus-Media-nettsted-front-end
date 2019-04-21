2de6a1104aed648719c2a553118480981842af7e
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-21 17:01:47 +0200

    Check if field exists in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./footer.scss";
+import IconText from "../icon-text/IconText";
 
 class Footer extends React.Component {
   componentWillMount() {
@@ -14,23 +15,73 @@ class Footer extends React.Component {
     }
   }
   render() {
+    let footerLogo = this.props["img"];
+    if (footerLogo) {
+      footerLogo = (
+        <img
+          src={footerLogo.url_full}
+          alt={footerLogo.attribute_alt}
+          className="headerIcon"
+        />
+      );
+    }
+
+    let footerP1;
+    let footerP2;
+    let footerP3;
+    if (this.props["p"]) {
+      footerP1 = this.props["p"][0];
+      if (footerP1) {
+        footerP1 = <p>{footerP1}</p>;
+      }
+      footerP2 = this.props["p"][1];
+      if (footerP2) {
+        footerP2 = <p>{footerP2}</p>;
+      }
+      footerP3 = this.props["p"][2];
+      if (footerP3) {
+        footerP3 = <p>{footerP3}</p>;
+      }
+    }
+
+    let footerAdress = null;
+    this.state.children.map(child => {
+      if (child.type == IconText && footerAdress == null) {
+        // Save component in variable
+        footerAdress = child;
+
+        // Remove component from the children array
+        const indexOfChild = this.state.children.indexOf(child);
+        this.state.children.splice(indexOfChild, 1);
+      }
+    });
+
+    let footerA1;
+    let footerA2;
+    if (this.props["a_int"]) {
+      footerA1 = this.props["a_int"][0];
+      if (footerA1) {
+        footerA1 = <a href="#">{footerA1}</a>;
+      }
+      footerA2 = this.props["a_int"][1];
+      if (footerA2) {
+        footerA2 = <a href="#">{footerA2}</a>;
+      }
+    }
     return (
       <footer className="footer">
         <div className="footer-inner layout-boxed">
           <section className="footer-inner-left">
             <div className="footer-inner-left-logo">
-              <img
-                src={this.props.bilde.url_full}
-                alt={this.props.bilde.attribute_alt}
-              />
-              <p>{this.props["tekst"][0]}</p>
+              {footerLogo}
+              {footerP1}
             </div>
             <div className="footer-inner-left-links">
-              <a href="#">{this.props["intern-link"][0]}</a>
-              <a href="#">{this.props["intern-link"][1]}</a>
-              <a href="#">{this.props["intern-link"][2]}</a>
+              {footerAdress}
+              {footerA1}
+              {footerA2}
             </div>
-            <p>{this.props["tekst"][1]}</p>
+            {footerP2}
           </section>
           <section className="footer-inner-right">
             {this.state.children.map(child => {
@@ -38,9 +89,7 @@ class Footer extends React.Component {
             })}
           </section>
         </div>
-        <div className="footer-copyright">
-          <p>{this.props["tekst"][2]}</p>
-        </div>
+        <div className="footer-copyright">{footerP3}</div>
       </footer>
     );
   }
