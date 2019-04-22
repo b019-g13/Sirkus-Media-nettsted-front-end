6aef3a1b509d471614af717914f975d90ccd36d4
Line Sharina Aamodt Hagen <line@datahjelpen.no>
2019-04-22 11:47:00 +0200

    Add menu in header

--- src/components/header-menu/HeaderMenu.js
+++ src/components/header-menu/HeaderMenu.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./header-menu.scss";
 import Axios from "axios";
+import logo from "../../logo.svg";
+import menuIcon from "../../menu.svg";
 
 class HeaderMenu extends React.Component {
   componentWillMount() {
@@ -26,14 +28,37 @@ class HeaderMenu extends React.Component {
   render() {
     const menuLinks = this.state.pageMenusLinks;
     return (
-      <nav className="navTop">
-        {menuLinks.map(function(links, i) {
-          return (
-            <a href={links.page_id} key={i}>
-              {links.name}
-            </a>
-          );
-        })}
+      <nav className="nav-top">
+        <section className="nav-top-left">
+          <img src={logo} alt="Logo: Sirkus Media" />
+        </section>
+        <section className="nav-top-right">
+          <button id="nav-top-right-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-menu"
+            >
+              <line x1="3" y1="12" x2="21" y2="12" />
+              <line x1="3" y1="6" x2="21" y2="6" />
+              <line x1="3" y1="18" x2="21" y2="18" />
+            </svg>
+          </button>
+          {menuLinks.map(function(links, i) {
+            return (
+              <a href={links.page_id} key={i}>
+                {links.name}
+              </a>
+            );
+          })}
+        </section>
       </nav>
     );
   }
