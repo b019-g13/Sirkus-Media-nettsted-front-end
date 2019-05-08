6665d15aa410338e16a3ec6ffae2837557c459db
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-05-08 17:52:53 +0200

    Update navigation. Add mobile nav. Add white version of logo

--- src/components/header-menu/HeaderMenu.js
+++ src/components/header-menu/HeaderMenu.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./header-menu.scss";
 import Axios from "axios";
-import logo from "../../logo.svg";
+import logo from "../../logo-white.svg";
 import menuIcon from "../../menu.svg";
 
 class HeaderMenu extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      navOpen: false
+    };
+
+    this.navToggle = this.navToggle.bind(this);
+  }
+
+  navToggle() {
+    this.setState({
+      navOpen: !this.state.navOpen
+    });
+  }
+
   componentWillMount() {
     this.getPageMenus();
     this.setState({
@@ -27,39 +43,65 @@ class HeaderMenu extends React.Component {
 
   render() {
     const menuLinks = this.state.pageMenusLinks;
+
+    const iconOpen = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon open"
+      >
+        <line x1="3" y1="12" x2="21" y2="12" />
+        <line x1="3" y1="6" x2="21" y2="6" />
+        <line x1="3" y1="18" x2="21" y2="18" />
+      </svg>
+    );
+
+    const iconClose = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="icon close"
+      >
+        <line x1="18" y1="6" x2="6" y2="18" />
+        <line x1="6" y1="6" x2="18" y2="18" />
+      </svg>
+    );
+
     return (
-      <nav className="nav-top">
+      <nav className={"nav-top" + (this.state.navOpen ? " nav-open" : "")}>
         <section className="nav-top-left">
           <a href="/">
             <img src={logo} alt="Logo: Sirkus Media" />
           </a>
         </section>
         <section className="nav-top-right">
-          <button id="nav-top-right-btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="white"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-menu"
-            >
-              <line x1="3" y1="12" x2="21" y2="12" />
-              <line x1="3" y1="6" x2="21" y2="6" />
-              <line x1="3" y1="18" x2="21" y2="18" />
-            </svg>
+          <button id="nav-top-right-btn" onClick={this.navToggle}>
+            {iconOpen}
+            {iconClose}
           </button>
-          {menuLinks.map(function(links, i) {
-            return (
-              <a href={links.page_id} key={i}>
-                {links.name}
-              </a>
-            );
-          })}
+          <div className="links">
+            {menuLinks.map(function(links, i) {
+              return (
+                <a href={links.page_id} key={i}>
+                  {links.name}
+                </a>
+              );
+            })}
+          </div>
         </section>
       </nav>
     );
